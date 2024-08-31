ecbda9a26cebca9ad12c47897a861a9d3991b764
ronecastro <ronecastro@hotmail.com>
2024-08-31 11:24:24 -0300

    added test_mode variable

--- monitor.py
+++ monitor.py
@@ -15,7 +15,9 @@ from utils import call_wapp as cw
 def evaluate():
     # make full PV list and create modem object
     f = FullPVList()
-    fullpvlist, modem = prepare_evaluate(f)
+    # don't try to load modem resource if in test mode
+    test_mode = True
+    fullpvlist, modem = prepare_evaluate(f, test_mode=test_mode)
     loop_index = 0
     print("Running!")
     # load notification db
@@ -58,7 +60,8 @@ def evaluate():
                         update_db= True # update notification database
                         update_log = True # write to log.txt
                         no_text = False # force notification text to none
-                        send_sms = True # send through modem
+                        # set if send through modem
+                        send_sms = (False if test_mode else True)
                         send_wapp = True # send through WhatsApp
                         print_msg = False #print sent sms text to terminal
                         byebye(ans, n, now, app_notifications, users_db, update_db=update_db, update_log=update_log, no_text=no_text, send_sms=send_sms, send_wapp=send_wapp, print_msg=print_msg, queue=n_queue)
