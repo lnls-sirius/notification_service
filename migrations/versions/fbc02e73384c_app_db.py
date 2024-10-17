ddb2d6980f932cedfc3ac10e707c6fe3baf23952
ronecastro <ronecastro@hotmail.com>
2024-10-17 14:43:19 -0300

    added docker run example for mount option

--- migrations/versions/fbc02e73384c_app_db.py
+++ migrations/versions/fbc02e73384c_app_db.py
@@ -1,8 +1,8 @@
 """app_db
 
-Revision ID: e385c2962c8d
+Revision ID: fbc02e73384c
 Revises: 
-Create Date: 2024-10-08 14:44:27.349790
+Create Date: 2024-10-17 12:06:20.449030
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e385c2962c8d'
+revision = 'fbc02e73384c'
 down_revision = None
 branch_labels = None
 depends_on = None
