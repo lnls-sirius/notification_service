b2b83fc7fad3d07229bafba91017a9488c457d64
ronecastro <ronecastro@hotmail.com>
2024-11-10 10:29:27 -0300

    force sms sending to True

--- utils.py
+++ utils.py
@@ -361,7 +361,7 @@ def call_modem(number, text2send, n_id, update_db_ans, update_log, username, ema
         msg = deepcopy(text2send[:-2])
         modem = Modem()
         modem.initialize()
-        modem_ans = modem.sendsms(number=number, msg=msg, force=False)
+        modem_ans = modem.sendsms(number=number, msg=msg, force=True)
         modem.closeconnection()
     else:
         modem_ans = 1, m_now
