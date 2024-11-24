52a16e988a1c98e61f1056cc0ae8c993ca6e7579
ronecastro <ronecastro@hotmail.com>
2024-11-24 17:06:30 -0300

    added status reseted to modem answer

--- utils.py
+++ utils.py
@@ -595,7 +595,15 @@ def ns_queuer(n_queue, writer_queue, busy_modem, busy_wapp, exit, system_errors,
                             app_notifications = app_db_("notifications")
                             update_db_ans = app_notifications.update(n_id, "last_sent", modem_ans[1])
                     else:
-                        item[11] = False
+                        if modem_ans[0] == "RESETED":
+                            modem_ans2 = modem_ans = call_modem(number, text2send, n_id, update_log, username, email, send_sms, now, print_msg, busy_modem, writer_queue, system_errors)
+                            n_queue.remove(item)
+                            if update_db == True:
+                                # update notification last_sent key
+                                app_notifications = app_db_("notifications")
+                                update_db_ans = app_notifications.update(n_id, "last_sent", modem_ans[1])
+                        else:
+                            item[11] = False
         system_errors_len = len(system_errors)
         if system_errors_len > 0:
             m_now = dt.now()
